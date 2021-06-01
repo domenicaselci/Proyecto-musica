97a7b98dab6622b36b5efb6ba016cd2703d894d1
domenicaselci <domenicaselci2002@gmail.com>
2021-06-01 19:27:37 -0300

    añado lista

--- javascript/detailalbum.js
+++ javascript/detailalbum.js
@@ -1,60 +1,99 @@
 window.addEventListener("load", function () {
 
-    let cancionesAlbum = document.querySelector("#contenidoprincipal");
-
     fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/album/302127")
-        .then(function (response) {
-            return response.json();
+        .then(function (respuesta) {
+            return respuesta.json();
         })
         .then(function (datos) {
             let tracksAlbum = datos;
-            for (let i = 0; i < tracksAlbum.length; i++) {
-                let imagenAlbum = tracksAlbum[0].cover_big;
-                let nombreArtista = tracksAlbum[1].contributors.name;
-                let tracksDos = tracksAlbum[0].title;
-                let nombresCanciones = tracksAlbum[2].tracks.data.title;
-                cancionesAlbum.innerHTML += `
-          <article class="contenido1">
-            <div class="primeraparte">
-            <figure>
-            <img src="${imagenAlbum}" alt="${tracksDos}">
-            </figure>
-            <div class="listagris">
-                <ul class="albumtim">
-                    <li class="album">ALBUM</li>
-                    <li class="tim">${tracksDos}</li>
-                </ul>
-                <ul class="listaheaven">
-                    <li class="artista">${nombreArtista}</li>
-                    <li>Electrónica</li>
-                    <li>Fecha de lanzamiento: 6 de junio de 2019</li>
-                </ul>
-            </div>
-        </div>
-            <ul class="fav">
-                <li class="icons"><a href="#" title="Añadir a favoritos" class="iconitos"><i
-                            class="far fa-heart"></i></a></li>
-                <li class="icons"><a href="#" title="Descargar álbum" class="iconitos"><i
-                            class="fas fa-angle-double-down"></i></i></i></i></a></li>
-                <li class="icons"><a href="playlist.html" title="Ir a mi playlist" class="iconitos"><i
-                            class="fas fa-compact-disc"></i></a></i></li>
-            </ul>
-        </article>         
 
-        <article class="articletim" id= "cancionesalbum">
-            <!--<ol>
-                <li class="cancionesconplay">
-                    <div><a href="#"><i class="fas fa-play" title="Play"></i> ${nombresCanciones}</a></div>
+            for (let i = 0; i < 1; i++) {
+                let albumPrimero = tracksAlbum;
+                let tituloAlbum = albumPrimero.title
+                //console.log(tituloAlbum);
+                let imagenAlbum = albumPrimero.cover_xl;
+                let nombreArtista = albumPrimero.artist.name;
+                let fecha = albumPrimero.release_date;
+                let primeraParte = document.querySelector(".primeraparte")
+                primeraParte.innerHTML += `
+                <figure>   
+                <img src="${imagenAlbum}" alt="${tituloAlbum}">
+                </figure>
+                <div class="listagris">
+                    <ul class="albumtim">
+                        <li class="album">ALBUM</li>
+                        <li class="tim">${tituloAlbum}</li>
+                    </ul>
+                    <ul class="listaheaven">
+                        <li class="artista">${nombreArtista}</li>
+                        <li>Electrónica</li>
+                        <li>Fecha de lanzamiento: ${fecha}</li>
+                    </ul>
+    
+            `
+                let nuevosTracks = albumPrimero.tracks.data;
+                let lista = document.querySelector("#cancionesyalbum ol");
+                for (let i = 0; i < nuevosTracks.length; i++) {
+                    lista.innerHTML += `<li class="cancionesconplay">
+                    <div><a href="#"><i class="fas fa-play" title="Play"></i> ${nuevosTracks[i].title} </a></div>
                     <a href="#" title="Añadir a mi playlist" class="mas"><i class="fas fa-ellipsis-h"></i></a>
                 </li>
-                 `
+                   `
+                }
             }
-
         })
         .catch(function (error) {
             console.log(error);
         })
 
+    let segundaParte = document.querySelector("#masde");
+    //console.log(segundaParte)
+    fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/3135556")
+        .then(function (respuesta) {
+            return respuesta.json();
+        })
+        .then(function (datos) {
+            let mas = datos;
+            let nombreDos = mas.artist.name;
+            let nombreMas = document.querySelector("#masde")
+            nombreMas.innerHTML += `<h1 class="titulodiscografia"> Más de ${nombreDos}</h1>`
+            for (let i = 0; i < 3; i++) {
+                let nombreCancion = mas.title;
+                let img = mas.contributors[0].picture_xl;
+                segundaParte.innerHTML += `
+                <article class="discos">
+                    <img src="${img}" alt="${nombreCancion}">
+                    <h3 class="albumesmartin"><a href="detail-track.html">${nombreCancion}</a></h3>
+                    <h4 class="subtitulosalbum"> Track</h4>
+                </article>
+                `
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
 
-
+        let terceraParte = document.querySelector("#relacionados")
+        fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart")
+        .then(function (respuesta) {
+            return respuesta.json();
+        })
+        .then(function (datos) {
+            let tres = datos.tracks;
+                       
+            for (let i = 0; i < 3; i++) {
+                let nombreTres = tres.data[i].artist.name;
+                let imgTres = tres.data[i].artist.picture_xl;
+                terceraParte.innerHTML += `
+                <article class="discos">
+                    <img src="${imgTres}" alt="${nombreTres}" class="imagenesartistas">
+                    <h3 class="albumesmartin"><a href="detail-artist.html">${nombreTres}</a></h3>
+                    <h4 class="subtitulosalbum">Artista</h4>
+                </article>
+                `
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
 })
