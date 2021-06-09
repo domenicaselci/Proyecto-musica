f39d106011b8b5146305123a02d35581940ec612
francoleone23 <fleone@udesa.edu.ar>
2021-06-09 10:06:57 -0300

    Update playlist.js

--- javascript/playlist.js
+++ javascript/playlist.js
@@ -1,3 +1,18 @@
-window.addEventListener("load", function(){}
+let recuperoStorage = localStorage.getItem('playlist');
+let playlist = JSON.parse(recuperoStorage);
+
+let playlistx = document.querySelector('.playlistx');
+console.log(recuperoStorage);
+if(recuperoStorage == null || recuperoStorage == "[]"){
+    playlist = [];
+
+    playlistx.innerHTML += '<li> No hay canciones en tu playlist </li>';
+    console.log(playlistx);
     
-    )
+} else {
+
+    playlist.forEach(function(idTrack){
+        buscarYMostrarTrack(idTrack);
+    });
+}
+
