652940e966bc7fcd9d4aac1ba2d3cd54f74a5abe
domenicaselci <domenicaselci2002@gmail.com>
2021-06-11 18:25:21 -0300

    searchresults

---
+++ javascript/searchresults.js
@@ -0,0 +1,63 @@
+window.addEventListener("load", function () {
+
+    let queryString = location.search;
+    console.log(queryString)
+    let queryStringObj = new URLSearchParams(queryString);
+    let filtro = queryStringObj.get("filtro");
+    let busqueda = queryStringObj.get("q");
+    console.log(filtro)
+    console.log(busqueda)
+
+    if (filtro == "todo") {
+        fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=${busqueda}`)
+            .then(function (respuesta) {
+                return respuesta.json()
+            }).then(function (datos) {
+
+                let todo = datos.data;
+                document.querySelector(".resultadosbusqueda").innerText = `Resultados para "${busqueda}"`
+                for (let i = 0; i < 1; i++) {
+                    let nombreArt = todo[i].artist.name;
+                    let cancion = todo[i].title;
+                    let imagenArtista = todo[i].artist.picture_xl;
+
+                    document.querySelector("#primero").innerHTML += `<img src="${imagenArtista}" alt="${nombreArt}">
+                <div class="titulossearch">
+                    <h3 class="electronica"><a href="genres.html">${nombreArt}</a></h3>
+                    <h4>Artista</h4>
+                </div>`
+                    for (let i = 0; i < 3; i++) {
+                        let nomAlb = todo[i].album.title
+                        let fotoAlb = todo[i].album.cover_xl
+                        document.querySelector("#segundo").innerHTML +=
+                            `
+                    <img src="${fotoAlb}" alt="${nomAlb}">
+                <div class="titulossearch">
+                    <h3 class="electronica"><a href="genres.html">${nomAlb}</a></h3>
+                    <h4>Álbum</h4>
+                </div>`
+                    }
+
+                }
+            }).catch(function (error) {
+                console.log(error)
+            })
+    } else if (filtro == "artista") {
+        fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/search/artist?q=${busqueda}`)
+            .then(function (respuesta) {
+                return respuesta.json()
+            }).then(function (datos) {
+
+            }).catch(function (error) {
+                console.log(error);
+            })
+    } else if (filtro == "track") {
+
+    }
+
+
+
+
+
+
+})
